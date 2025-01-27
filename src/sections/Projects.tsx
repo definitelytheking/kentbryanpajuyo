6a103d276b6e33af98f2b504cc5430d978ced3ef
definitelytheking <bryanpajuyo@gmail.com>
2025-01-27 21:03:44 +0800

    fixed stuff

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -81,8 +81,11 @@ export const ProjectsSection = () => {
                   </h3>
                   <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
                   <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                    {project.results.map((result) => (
-                      <li className="flex gap-2 text-sm md:text-base text-white/50">
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm md:text-base text-white/50"
+                      >
                         <CheckCircleIcon className="size-5 md:size-6" />
                         <span>{result.title}</span>
                       </li>
